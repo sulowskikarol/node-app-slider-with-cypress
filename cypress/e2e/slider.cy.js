f17e32666490863b21dfefd1037d8dc937d5e9c9
sulowskikarol <karolsu128@gmail.com>
2025-03-25 18:28:34 +0100

    New cypress test and hotjar config added

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -16,3 +16,28 @@ describe('Swiper Gallery Test', function () {
     cy.get('.swiper-slide-active').should('contain', 'Paris');
   });
 });
+
+describe('Swiper Gallery Test', function () {
+  it('Checks if all slides have valid description', function () {
+    const slides = [
+      { title: 'Rome', description: 'Italy' },
+      { title: 'London', description: 'United Kingdom' },
+      { title: 'Paris', description: 'France' },
+    ];
+
+    cy.visit('http://localhost:3000');
+    
+    slides.forEach((slide, index) => {
+      if (index !== 0) {
+        cy.get('.swiper-button-next').click();
+        cy.wait(2000);
+      }
+      cy.get('.swiper-slide-active .card-description h1')
+        .should('be.visible')
+        .and('have.text', slide.title);
+      cy.get('.swiper-slide-active .card-description p')
+        .should('be.visible')
+        .and('have.text', slide.description);
+    });
+  });
+});
